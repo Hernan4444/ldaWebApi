d35a87df894469e190a0b75e8a1cd76c162dc09d
Hernan Valdivieso <hfvaldivieso@uc.cl>
2019-02-16 13:14:49 -0300

    Deploy

---
+++ flask_heroku.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+from flask import Flask, request, json, Response
+from json import loads as load_json
+import os
+import re
+from functions import load_database, run_lda, run_interactive_lda
+from pathlib import Path  # python3 only
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+TOKEN = os.getenv("token")
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/pin", methods=["POST", "GET"])
+def pin():
+    load_database()
+    return "pin"
+
+
+@app.route("/lda", methods=["POST", "GET"])
+def test():
+    token = request.form.get('token')
+    if token != TOKEN:
+
+        response = Response(json.dumps({'error': 'No tienes autorización'}),
+                            status=401,
+                            mimetype='application/json'
+                            )
+        return response
+
+    iterations = int(request.form.get('iterations'))
+    mode = request.form.get('mode')
+    alpha = float(request.form.get('alpha', "0").replace(",", "."))
+    beta = float(request.form.get('beta', "0").replace(",", "."))
+    topics = int(request.form.get('topics'))
+    database = request.form.get('database')
+
+    if database == "NewGroups.5":
+        data = load_database(5)
+    elif database == "NewGroups.10":
+        data = load_database(10)
+    else:
+        data = load_database()
+
+    if mode == "LDA":
+        result = run_lda(data, iterations, alpha, beta, topics)
+    else:
+        nu = float(request.form.get('nu', "0").replace(",", "."))
+        seed = json.loads(request.form.get('seeds', "[]"))
+        result = run_interactive_lda(data, iterations, alpha, beta, nu, topics, seed, mode)
+
+    # return string_r
+    response = Response(json.dumps(result),
+                        status=200,
+                        mimetype='application/json'
+                        )
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ functions.py
@@ -0,0 +1,67 @@
+import lda
+import numpy as np
+import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.datasets import fetch_20newsgroups
+
+
+def load_database(option=None):
+
+    if option == 5:
+        categ = ['alt.atheism', 'comp.graphics', 'rec.autos', 'rec.sport.hockey', 'sci.med']
+    elif option == 10:
+        categ = ['alt.atheism', 'comp.graphics', 'comp.windows.x', 'misc.forsale', 'rec.autos',
+                 'rec.sport.hockey', 'sci.crypt', 'sci.space', 'soc.religion.christian', 'talk.politics.guns', ]
+    else:
+        categ = None
+
+    return fetch_20newsgroups(subset='train', remove=(
+        'headers', 'footers', 'quotes'), categories=categ)
+
+
+def run_lda(newsgroups_train, iterations, alpha, eta, topics):
+
+    tf_vectorizer = CountVectorizer(stop_words='english', max_features=1000, min_df=2, max_df=0.9)
+    tf = tf_vectorizer.fit_transform(newsgroups_train.data)
+
+    model = lda.LDA(n_topics=topics, n_iter=iterations, alpha=alpha, eta=eta, refresh=50)
+    model.fit(tf)
+
+    topics = []
+    for topic_dist in model.topic_word_:
+        topics.append(list(zip(
+            ["{:0.5f}".format(x) for x in np.array(topic_dist)[np.argsort(topic_dist)][:-20:-1]],
+            np.array(tf_vectorizer.get_feature_names())[np.argsort(topic_dist)][:-20:-1]
+        )))
+
+    return topics
+
+
+def run_interactive_lda(newsgroups_train, iterations, alpha, eta, nu, topics, seeds, mode):
+
+    tf_vectorizer = CountVectorizer(stop_words='english', max_features=1000, min_df=2, max_df=0.9)
+    tf = tf_vectorizer.fit_transform(newsgroups_train.data)
+    vocab = tf_vectorizer.get_feature_names()
+
+    new_seeds = []
+
+    for constrain in seeds:
+        new_topic = []
+        for word in constrain:
+            if word in vocab:
+                new_topic.append(vocab.index(word))
+        new_seeds.append(new_topic)
+
+    model = lda.LDA(n_topics=topics, n_iter=iterations, alpha=alpha,
+                    eta=eta, nu=nu, seed=new_seeds, refresh=100, mode=mode.lower())
+                    
+    model.fit(tf)
+
+    topics = []
+    for topic_dist in model.topic_word_:
+        topics.append(list(zip(
+            ["{:0.5f}".format(x) for x in np.array(topic_dist)[np.argsort(topic_dist)][:-20:-1]],
+            np.array(tf_vectorizer.get_feature_names())[np.argsort(topic_dist)][:-20:-1]
+        )))
+
+    return topics
