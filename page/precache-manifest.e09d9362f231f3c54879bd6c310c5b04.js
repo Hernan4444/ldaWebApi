c9122eacf9d4a564aa5e0e947dc99131bbc3077a
Hernan Valdivieso <hfvaldivieso@uc.cl>
2019-12-07 17:50:14 -0300

    page web

---
+++ page/precache-manifest.e09d9362f231f3c54879bd6c310c5b04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "27f4f44ec44bb974a010eb456567709f",
+    "url": "/ldaApi/static/media/logo.27f4f44e.png"
+  },
+  {
+    "revision": "4e4ddc8c402295893fb881d4bcc43fad",
+    "url": "/ldaApi/static/media/UnicaOne-Regular.4e4ddc8c.ttf"
+  },
+  {
+    "revision": "8625837a60ad14e4715c2908647e1342",
+    "url": "/ldaApi/static/media/HelveticaNeue-Thin.8625837a.otf"
+  },
+  {
+    "revision": "3900101b551547ea2b82",
+    "url": "/ldaApi/static/js/runtime~main.3900101b.js"
+  },
+  {
+    "revision": "e1e54042881c4c8a8f77",
+    "url": "/ldaApi/static/js/main.e1e54042.chunk.js"
+  },
+  {
+    "revision": "e482bc70ae4c66a76918",
+    "url": "/ldaApi/static/js/1.e482bc70.chunk.js"
+  },
+  {
+    "revision": "e1e54042881c4c8a8f77",
+    "url": "/ldaApi/static/css/main.dd67a610.chunk.css"
+  },
+  {
+    "revision": "e482bc70ae4c66a76918",
+    "url": "/ldaApi/static/css/1.05418b53.chunk.css"
+  },
+  {
+    "revision": "ef77c26d50792aa365624bdfd289e6c6",
+    "url": "/ldaApi/index.html"
+  }
+];
